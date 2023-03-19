533e03d4ae319ba7ca4258c97c3ce48f9bf9e700
Abeldleon <abelefai@gmail.com>
2023-03-18 19:26:36 -0700

    added posts

--- habla/models.py
+++ habla/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
+class Post(models.Model):
+
+    options = {
+        ("draft", "Draft"),
+        ("published", "published"),
+    }
+
+    title = models.CharField(max_length=200)
+
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    status = models.CharField(max_length=10, choices=options, default="draft")
+    
+    class Meta:
+        ordering = ("-created_at",)
+    def __str__(self):
+        return self.title
+
+
+    
 # Create your models here.
--- habla/views.py
+++ habla/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import ListView
+
+from .models import Post
 # Create your views here.
 def chat(request):
-    return render(request, 'chat.html', {'name': 'Mosh'})
+    model = Post.objects.all()
+    return render(request, 'index.html', {'posts': model})
+
+def create_post(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        post = Post(title=title, content=content)
+        post.save()
+        return redirect('blog_post_list')
+    else:
+        return render(request, '/create_post.html')
